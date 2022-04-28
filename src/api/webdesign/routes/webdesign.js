f49d8d59f774e35e8161ecd2f9de9e25c551d178
Glenn <glenn.reumers@hotmail.com>
2022-04-28 15:51:57 +0200

    Add webdesign single type

---
+++ src/api/webdesign/routes/webdesign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * webdesign router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::webdesign.webdesign');
